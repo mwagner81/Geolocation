7b75bde962375bbafad1ebc0052acc31c8d2f195
mwagner81 <wagner@idsolutions.at>
2013-10-10 13:26:46 +0200

    Error

--- www/js/footerScript.js
+++ www/js/footerScript.js
@@ -15,7 +15,23 @@ function getPCurGeoError(error) {
     var d = new Date();
     var span_date = '<span>Zeit: ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + '</span> '; 
     
-    element.innerHTML = '<span><b>Permanent Error</b></span> '+ span_date + '<br><hr>'+ element.innerHTML;
+//    element.innerHTML = '<span><b>Error</b></span> '+ span_date + '<br><hr>'+ element.innerHTML;
+    
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            element.innerHTML = "<span><b>Error</b></span> "+ span_date + "<br>User denied the request for Geolocation.<br><hr>";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            element.innerHTML = "<span><b>Error</b></span> "+ span_date + "<br>Location information is unavailable..<br><hr>";
+            break;
+        case error.TIMEOUT:
+            element.innerHTML = "<span><b>Error</b></span> "+ span_date + "<br>The request to get user location timed out.<br><hr>";
+            break;
+        case error.UNKNOWN_ERROR:
+            element.innerHTML = "<span><b>Error</b></span> "+ span_date + "<br>An unknown error occurred.<br><hr>";
+            break;
+    }
+    
 }   
     
 function onPermGeoDataSuccess(position) {
@@ -23,7 +39,7 @@ function onPermGeoDataSuccess(position) {
     var span_date = '<span>Zeit: ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() +'</span>';      
     
     element.innerHTML = '<span><b>Success</b></span> | '+ span_date      + '<br />' +
-        'Lat: '          + position.coords.latitude          + '<br />' +
-        'Long: '         + position.coords.longitude         + '<br />' +
-        'Acc: '          + position.coords.accuracy          + '<br><hr>'+ element.innerHTML;
+    'Lat: '          + position.coords.latitude          + '<br />' +
+    'Long: '         + position.coords.longitude         + '<br />' +
+    'Acc: '          + position.coords.accuracy          + '<br><hr>'+ element.innerHTML;
 }
